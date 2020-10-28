2e1398538fe1c3b0caf5e7328f085ba06e6eb865
mqdsg@qq.com <mqdsg@qq.com>
2020-10-28 17:19:50 +0800

    wpfdump

---
+++ dump/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dump
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = null;
+            // NullPointerException will happen in next line
+            Console.WriteLine("{0}-{1}", p.ID, p.Name);
+            Console.ReadKey();
+        }
+    }
+}
